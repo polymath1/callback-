9cad043509e62ebcf51ce6837a86e670630901f9
polymath1 <vino_sivajothy@hotmail.com>
2019-02-08 01:27:23 +0000

    Array Mapping Challenge

---
+++ anonymousFunctions.js
@@ -0,0 +1,86 @@
+var input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+var result = input.map(function(lola){
+  return(Math.sqrt(lola.x * lola.x + lola.y * lola.y))
+}
+
+
+
+
+  );
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
